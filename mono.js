1f1a5d9e72d3ce4f35da90e8b0c11598d4be8db9
Sonu-Syed <135698326+Sonu-Syed@users.noreply.github.com>
2024-08-16 10:14:38 +0530

    Add files via upload

---
+++ mono.js
@@ -0,0 +1,17 @@
+
+let food1=document.getElementById('food1');
+let food2=document.getElementById('food2');
+let food3=document.getElementById('food3');
+let food=document.getElementById('food');
+
+ 
+food1.addEventListener('click',()=>{
+    food1.style.backgroundImage="url('https://tse2.mm.bing.net/th?id=OIP.kVYQkvM_93PGhLs52Kq41gHaHa&pid=Api&P=0&h=180')";
+});
+food2.addEventListener('click',()=>{
+    food2.style.backgroundImage="url('https://tse4.mm.bing.net/th?id=OIP.2VSklv9bxgutFX2ZmeqSgwHaE8&pid=Api&P=0&h=180')";
+});
+food3.addEventListener('click',()=>{
+    food3.style.backgroundImage="url('https://tse2.mm.bing.net/th?id=OIP.y2UEgtXsNzO8O9_NInBGIQHaHa&pid=Api&P=0&h=180')";
+});
+
